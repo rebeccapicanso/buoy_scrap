c0712582125349f5fd43cf6c7f57e53b006136f3
Rebecca Picanso <76741837+rebeccapicanso@users.noreply.github.com>
2024-02-24 16:09:15 -0500

    Update buoygrab.py
    cleaned up buoygrab to grab every hour from camera z64a

--- buoygrab.py
+++ buoygrab.py
@@ -2,60 +2,34 @@ import os.path
 from bs4 import BeautifulSoup as bs
 import requests
 import os, time
+import cv2
+import numpy as np
 
-# url = 'https://www.ndbc.noaa.gov/images/buoycam/Z32A_2023_10_27_0210.jpg'
-# page = requests.get(url)
+
+# this is tracking a single buoy off the coast of the Aleutian Islands!
+# can be expanded to include all 54 buoys, but not recommended unless you are uploading images to
+# google cloud storage or something similar.
 
 # set timezone for Aleutian
 os.environ['TZ'] = 'US/Aleutian'
 time.tzset()
-day = time.strftime("%d")
 
-# grab image from webpage
-dynamic_url_Z64A = 'https://www.ndbc.noaa.gov/images/buoycam/Z64A_2024_01_' + day + '_2010.jpg'
-img_data_Z64A = requests.get(dynamic_url_Z64A).content
-jpg_name_Z64A = 'Z64A_2024_01_' + day + '_2010.jpg'
+# get yesterday's date
+yesterday = time.strftime("%d", time.gmtime(time.time() - 86400))
 
-with open(jpg_name_Z64A, 'wb') as handler:
-    handler.write(img_data_Z64A)
+# create an hour variable
+hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16',
+          '17', '18', '19', '20', '21', '22', '23']
 
-##### CHECK HUE VALUE #####
+# create a loop to grab images for each hour
 
-import cv2
-import numpy as np
+for x in hours:
+    dynamic_url_Z64A = 'https://www.ndbc.noaa.gov/images/buoycam/Z64A_2024_02_' + yesterday + '_' + x + '10.jpg'
+    img_data_Z64A = requests.get(dynamic_url_Z64A).content
 
-# read image
-img = cv2.imread(jpg_name_Z64A)
+    # read image and asssess HSV value
+    jpg_name_Z64A = 'Z64A_2024_02_' + yesterday + '_' + x + '10.jpg'
+    img = cv2.imread(jpg_name_Z64A)
 
-# convert img to hsv
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-h = hsv[:,:,0]
-s = hsv[:,:,1]
-v = hsv[:,:,2]
-
-# write the hue to a file
-np.savetxt('h.txt', h)
-
-# # shift the hue
-    
-# # cv2 will clip automatically to avoid color wrap-around
-# huechange = 10   # 0 is no change; 0<=huechange<=180
-# hnew = cv2.add(h, huechange)
-
-# # combine new hue with s and v
-# hsvnew = cv2.merge([hnew,s,v])
-
-# # convert from HSV to BGR
-# result = cv2.cvtColor(hsvnew, cv2.COLOR_HSV2BGR)
-
-# # save result
-# cv2.imwrite('result.png', result)
-    
-# # decide if you should keep or not depending on the type of hue
-    
-    
-
-# if huechange < 10:
-#     print("keep")
-# else:
-#     print("discard")
+    with open(jpg_name_Z64A, 'wb') as handler:
+        handler.write(img_data_Z64A)
